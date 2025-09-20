398555526e5c48439b32052a6035e7106c97f8ff
Gris <gehunelgbedji@gmail.com>
2025-09-21 00:05:24 +0100

    commitSentenceMaker

---
+++ sentenceMaker.js
@@ -0,0 +1,19 @@
+let adjective = "dangerous";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountains";
+let adjective2 = "large";
+let noun2 = "snakes";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: " + firstStory);
+adjective = "beautiful";
+noun = "Siren";
+verb = "getting attacked";
+place = "the Oceans";
+adjective2 = "minus";
+noun2 = "fishes";
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
